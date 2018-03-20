86fee89c17c1b7520e3c3b81245e715e45ad96d5
Sean-Hassett <hassetse@tcd.ie>
2018-03-20 17:31:16 +0000

    added rudimentary export function

--- project/src/App.js
+++ project/src/App.js
@@ -2,15 +2,28 @@ import React, { Component } from 'react';
 import { ScrollView } from 'react-native';
 import { Header } from './components/common';
 import SetupForm from './components/SetupForm';
+import DataView from './components/DataView';
 
 class App extends Component {
+    state = { dataEntered: true };
+
     render() {
-        return (
-            <ScrollView scrollEnabled={true}>
-                <Header headerText="Stereotypy App" />
-                <SetupForm />
-            </ScrollView>
-        );
+        switch (this.state.dataEntered) {
+            case false:
+                return (
+                    <ScrollView scrollEnabled={true}>
+                        <Header headerText="Stereotypy App" />
+                        <SetupForm />
+                    </ScrollView>
+                );
+            case true:
+                return (
+                    <ScrollView scrollEnabled={true}>
+                        <Header headerText="Stereotypy App" />
+                        <DataView />
+                    </ScrollView>
+                );
+        }
     }
 }
 
---
+++ project/src/components/DataView.js
@@ -0,0 +1,68 @@
+import React, { Component } from 'react';
+import { AsyncStorage, Text } from 'react-native';
+import { Button, Card, CardSection, Input } from './common';
+
+class DataView extends Component {
+    state = { name: '', dob: '', stereoRate: '', stereotypies: '', password: '', sessionNo: '', dataForExport: '' };
+
+    componentDidMount = () => this.getData()
+    
+    getData = () => {
+        AsyncStorage.getItem('name').then((value) => this.setState({ 'name': value }));
+        AsyncStorage.getItem('dob').then((value) => this.setState({ 'dob': value }));
+        AsyncStorage.getItem('stereoRate').then((value) => this.setState({ 'stereoRate': value }));
+        AsyncStorage.getItem('stereotypies').then((value) => this.setState({ 'stereotypies': value }));
+        AsyncStorage.getItem('password').then((value) => this.setState({ 'password': value }));
+    }
+
+    exportData = () => {
+        //TODO: loop according to sessionNo with \n at end of each
+
+        var name = this.state.name;
+        var dob = this.state.dob;
+        var stereoRate = this.state.stereoRate;
+        var stereotypies = this.state.stereotypies;
+        var password = this.state.password;
+
+        var dataForExport = `${name},${dob},${stereoRate},${stereotypies},${password}`;
+        this.setState({ 'dataForExport': dataForExport });
+    }
+    
+    render() {
+        return (
+            <Card>
+                <CardSection>
+                    <Text>Name: {this.state.name}</Text>
+                </CardSection>
+
+                <CardSection>
+                    <Text>Date of Birth: {this.state.dob}</Text>
+                </CardSection>
+
+                <CardSection>
+                    <Text>Rate: {this.state.stereoRate}</Text>
+                </CardSection>
+
+                <CardSection>
+                    <Text>Stereotypies: {this.state.stereotypies}</Text>
+                </CardSection>
+
+                <CardSection>
+                    <Text>Password(bad idea): {this.state.password}</Text>
+                </CardSection>
+
+                <CardSection>
+                    <Text>Everything: {this.state.dataForExport}</Text>
+                </CardSection>
+
+                <CardSection>
+                    <Button onPress={this.exportData.bind(this)}>
+                        Export
+                    </Button>
+                </CardSection>
+            </Card>
+        );
+    }
+}
+
+export default DataView;
